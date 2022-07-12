99396c3fe00d78ba10f1046b459bac0f651ec0c3
NANOSOFT <mohamedahmeds951@gmail.com>
2022-07-12 12:00:22 +0200

    س

--- TDO/GalaryCreateDTO.cs
+++ TDO/GalaryCreateDTO.cs
@@ -8,7 +8,7 @@ namespace ProductGallary.TDO
         [Required(ErrorMessage = "من فضلك ادخل اسم المعرض")]
         [MinLength(3, ErrorMessage = "يجب ان يكون الاسم اكثر من حرفين")]
         [MaxLength(20, ErrorMessage = "يجب ان يكون الاسم اقل من  عشرون حرفا")]
-        public string Name { get; set; }
+        public string name { get; set; }
 
         [Required(ErrorMessage = "من فضلك ادخل لوجو المعرض")]
         public IFormFile Logo { get; set; }
